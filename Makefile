ee26630d74476d99c83bcd336020916ff6a51cf0
Jose Coixao <zecoxao@hotmail.com>
2013-06-30 09:36:09 +0100

    removed hdd decryption, only generates slim keys
    not necessary anymore. better to use glevand's tools for that

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-Wall
 LDFLAGS=
-SOURCES=lib/aes.c lib/sha1.c lib/des.c lib/aes_omac.c lib/keys.c lib/kgen.c lib/aes_xts.c lib/util.c lib/indiv.c lib/eid.c lib/hdd.c lib/main.c
+SOURCES=lib/aes.c lib/sha1.c lib/des.c lib/aes_omac.c lib/keys.c lib/kgen.c lib/aes_xts.c lib/util.c lib/indiv.c lib/eid.c lib/main.c
 EXECUTABLE=decrypt_tools
 all:
 	$(CC) $(CFLAGS) $(SOURCES) $(LDFLAGS) -o $(EXECUTABLE)
--- lib/main.c
+++ lib/main.c
@@ -11,21 +11,16 @@
 #include "util.h"
 #include "keys.h"
 #include "eid.h"
-#include "hdd.h"
 #include "kgen.h"
 #include "types.h"
 #include "indiv.h"
 
-void decrypt_hdd()
+void generate_hdd_individuals()
 {
     u8 ata_k1[0x20], ata_k2[0x20], edec_k1[0x20], edec_k2[0x20];
-  u32 size;
-
+	
 	//fetching root_key
 	eid_root_key = _read_buffer((s8*)"data/eid_root_key", NULL);
-	
-	//Assuming sectors start with sector 0.
-	u8 *sectors = _read_buffer((s8*)"data/sectors", &size);
 
 	//Generate keys.
 	generate_ata_keys(eid_root_key, eid_root_key + 0x20, ata_k1, ata_k2);
@@ -35,22 +30,6 @@ void decrypt_hdd()
 	_hexdump(stdout, "ATA-TWEAK-KEY   ", 0, ata_k2, 0x20, 0);
 	_hexdump(stdout, "ENCDEC-DATA-KEY ", 0, edec_k1, 0x20, 0);
 	_hexdump(stdout, "ENCDEC-TWEAK-KEY", 0, edec_k2, 0x20, 0);
-
-	//Decrypt all sectors.
-	decrypt_sectors(sectors, 0, size, ata_k1, ata_k2, 1);
-
-	// we should make better a fn() with sector_start, sector_size params, and also to make
-	// 3rd arg the MIN() of "size", sector_size
-
-	//Decrypt VFLASH sectors starting at sector 8.
-	// decrypt_sectors(sectors+8*SECTOR_SIZE, 8, size - 8*SECTOR_SIZE, edec_k1, edec_k2, 0);
-	decrypt_sectors(sectors+8*SECTOR_SIZE, 8, size - 8*SECTOR_SIZE, edec_k1, edec_k2, 0);
-
-	//Decrypt GameOS (UFS2) sectors starting at sector 0x80010 (524304).
-	decrypt_sectors(sectors+524304*SECTOR_SIZE, 524304, size - 524304*SECTOR_SIZE, ata_k1, ata_k2, 1);
-
-	_write_buffer((s8*)"data/sectors_decrypted", sectors, size);
-
 }
 
 void decrypt_eid()
@@ -81,36 +60,6 @@ void encrypt_eid0_section_A()
 	eid0_encrypt_section_A((s8*)"eid/eid0decrypted",(s8*)"eid/eid0encrypted");
 }
 
-void encrypt_hdd()
-{
-    u8 ata_k1[0x20], ata_k2[0x20], edec_k1[0x20], edec_k2[0x20];
-	u32 size;
-	
-	//fetching root_key
-	eid_root_key = _read_buffer((s8*)"data/eid_root_key", NULL);
-
-	//Assuming sectors start with sector 0.
-	u8 *sectors = _read_buffer((s8*)"data/sectors_decrypted", &size);
-
-	//Generate keys.
-	generate_ata_keys(eid_root_key, eid_root_key + 0x20, ata_k1, ata_k2);
-	generate_encdec_keys(eid_root_key, eid_root_key + 0x20, edec_k1, edec_k2);
-
-	_hexdump(stdout, "ATA-DATA-KEY    ", 0, ata_k1, 0x20, 0);
-	_hexdump(stdout, "ATA-TWEAK-KEY   ", 0, ata_k2, 0x20, 0);
-	_hexdump(stdout, "ENCDEC-DATA-KEY ", 0, edec_k1, 0x20, 0);
-	_hexdump(stdout, "ENCDEC-TWEAK-KEY", 0, edec_k2, 0x20, 0);
-	
-	//Encrypt all sectors.
-	encrypt_sectors(sectors, 0, size, ata_k1, ata_k2, 1);
-
-	//Encrypt VFLASH sectors starting at sector 8.
-	encrypt_sectors(sectors+8*SECTOR_SIZE, 8, size - 8*SECTOR_SIZE, edec_k1, edec_k2, 0);
-
-	_write_buffer((s8*)"data/sectors_encrypted", sectors, size);
-
-}
-
 void syscon_auth()
 {
 	aes_context aes_ctxt;
@@ -136,28 +85,21 @@ void syscon_auth()
 int main()
 {
     int i;
-    printf("Select an option\n1-Decrypt HDD\n2-Encrypt HDD\n3-Decrypt eEID(missing eid5)\n4-Decrypt ALL\n5-Generate Syscon AUTH seeds(Acording to wiki)\n6-Encrypt EID0 Section A\n0-Exit\n");
+    printf("Select an option\n1-Decrypt eEID(missing eid5)\n2-Encrypt EID0 Section A\n3-Generate Syscon AUTH seeds(Acording to wiki)\n4-Generate HDD Keys(Slim)\n0-Exit\n");
     scanf("%d",&i);
     switch(i)
     {
         case 1:
-            decrypt_hdd();
-            break;
-        case 2:
-			encrypt_hdd();
-            break;
-        case 3:
             decrypt_eid();
 			break;
-        case 4:
-			decrypt_hdd();
-			decrypt_eid();
+        case 2:
+			encrypt_eid0_section_A();
 			break;
-		case 5:
+		case 3:
 			syscon_auth();
 			break;
-		case 6:
-			encrypt_eid0_section_A();
+		case 4:
+			generate_hdd_individuals();
 			break;
 		case 0:
             break;
